6a3a8de343c72af9e94a378e7c42afabf73f8135
Voltaire212 <cjimenez0288@gmail.com>
2024-11-25 12:02:11 -0500

    Add projects div
    Projects div has been added with smooth scale transition

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -32,14 +32,48 @@ const Home = () => {
           <div className="mainimageright">
             <p>Desarrollador frontend</p>
             <p>Énfasis React/React Native</p>
-            <p>
-              Javascript, Typescipt <h3>y más...</h3>
-            </p>
+            <p>Javascript, Typescipt</p>
+            <div className="mainimageright__hover">
+              y más...
+              <div className="mainimageright__hovermore">
+                <p>Java</p>
+                <p>android</p>
+                <p>IOs</p>
+                <p>Git/Github</p>
+                <p>Firebase</p>
+                <p>Figma</p>
+                <p>SQL</p>
+                <p>illustrator</p>
+              </div>
+            </div>
             <hr />
           </div>
         </div>
       </div>
       <button className="homescreenbutton">Acerca de mí</button>
+      <div className="homeprojects">
+        <hr />
+        <h1>Proyectos</h1>
+        <h3>En esta sección te dejo un link a mis proyectos más recientes</h3>
+        <div className="projectsquaresdiv">
+          <div className="projectsquare">
+            <h2>Proyecto</h2>
+            <p>Este es un proyecto</p>
+          </div>
+          <div className="projectsquare">
+            <h2>Proyecto</h2>
+            <p>Este es un proyecto</p>
+          </div>
+          <div className="projectsquare">
+            <h2>Proyecto</h2>
+            <p>Este es un proyecto</p>
+          </div>
+          <div className="projectsquare">
+            <h2>Proyecto</h2>
+            <p>Este es un proyecto</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
